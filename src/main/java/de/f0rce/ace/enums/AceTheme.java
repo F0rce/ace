f7aa9cd988dc92fd246d80fc6404365479e75e76
David Dodlek <david@dodlek.com>
2022-02-10 12:29:48 +0100

    style: formatting (google-java-format)

--- src/main/java/de/f0rce/ace/enums/AceTheme.java
+++ src/main/java/de/f0rce/ace/enums/AceTheme.java
@@ -1,15 +1,44 @@
 package de.f0rce.ace.enums;
 
-/**
- * @author David "F0rce" Dodlek
- */
-
+/** @author David "F0rce" Dodlek */
 public enum AceTheme {
-
-	ambiance, chaos, chrome, clouds, clouds_midnight, cobalt, crimson_editor, dawn, dracula, dreamweaver, eclipse,
-	github, gob, gruvbox, idle_fingers, iplastic, katzenmilch, kr_theme, kuroir, merbivore, merbivore_soft,
-	mono_industrial, monokai, nord_dark, pastel_on_dark, solarized_dark, solarized_light, sqlserver, terminal, textmate,
-	tomorrow, tomorrow_night, tomorrow_night_blue, tomorrow_night_bright, tomorrow_night_eighties, twilight,
-	vibrant_ink, xcode;
-
+  ambiance,
+  chaos,
+  chrome,
+  clouds,
+  clouds_midnight,
+  cobalt,
+  crimson_editor,
+  dawn,
+  dracula,
+  dreamweaver,
+  eclipse,
+  github,
+  gob,
+  gruvbox,
+  idle_fingers,
+  iplastic,
+  katzenmilch,
+  kr_theme,
+  kuroir,
+  merbivore,
+  merbivore_soft,
+  monokai,
+  mono_industrial,
+  nord_dark,
+  one_dark,
+  pastel_on_dark,
+  solarized_dark,
+  solarized_light,
+  sqlserver,
+  terminal,
+  textmate,
+  tomorrow,
+  tomorrow_night,
+  tomorrow_night_blue,
+  tomorrow_night_bright,
+  tomorrow_night_eighties,
+  twilight,
+  vibrant_ink,
+  xcode;
 }

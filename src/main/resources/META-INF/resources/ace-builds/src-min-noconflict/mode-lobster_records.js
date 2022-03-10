7198e8f85c568cc5efa936d6c79782add0df8650
David Dodlek <david@dodlek.com>
2022-03-10 11:57:54 +0100

    style: improved highlighting & styling

--- src/main/resources/META-INF/resources/ace-builds/src-min-noconflict/mode-lobster_records.js
+++ src/main/resources/META-INF/resources/ace-builds/src-min-noconflict/mode-lobster_records.js
@@ -1,41 +1,125 @@
 ace.define("ace/mode/lobster_records", function (require, exports, module) {
+  "use strict";
+
   var oop = require("ace/lib/oop");
-  var TextMode = require("ace/mode/text").Mode;
-  var LobsterRecordsHighlightRules =
-    require("ace/mode/lobster_records_highlight_rules").LobsterRecordsHighlightRules;
+  var TextHighlightRules =
+    require("ace/mode/text_highlight_rules").TextHighlightRules;
 
-  var Mode = function () {
-    this.HighlightRules = LobsterRecordsHighlightRules;
-  };
-  oop.inherits(Mode, TextMode);
+  var escapedRe =
+    "\\\\(?:x[0-9a-fA-F]{2}|" + // hex
+    "u[0-9a-fA-F]{4}|" + // unicode
+    "u{[0-9a-fA-F]{1,6}}|" + // es6 unicode
+    "[0-2][0-7]{0,2}|" + // oct
+    "3[0-7][0-7]?|" + // oct
+    "[4-7][0-7]?|" + //oct
+    ".)";
 
-  (function () {
-    // Extra logic goes here. (see below)
-  }.call(Mode.prototype));
+  var LobsterRecordsHighlightRules = function () {
+    this.$rules = {
+      start: [
+        {
+          token: "keyword.operator",
+          regex: "(\\((?=.+\\)))",
+          next: "handleOpeningBrace",
+        },
+        {
+          token: "constant.numeric",
+          regex: "\\d",
+        },
+        {
+          token: "string",
+          regex: "'(?=.)",
+          next: "qstring",
+        },
+        {
+          token: "string",
+          regex: '"(?=.)',
+          next: "qqstring",
+        },
+        {
+          token: "constant",
+          regex: "[Ll]ists:|[Mm]aps:|[Vv]ariables:",
+        },
+      ],
 
-  exports.Mode = Mode;
-});
+      qqstring: [
+        {
+          token: "constant.language.escape",
+          regex: escapedRe,
+        },
+        {
+          token: "string",
+          regex: "\\\\$",
+          consumeLineEnd: true,
+        },
+        {
+          token: "string",
+          regex: '"|$',
+          next: "no_regex",
+        },
+        {
+          defaultToken: "string",
+        },
+      ],
 
-ace.define(
-  "ace/mode/lobster_records_highlight_rules",
-  function (require, exports, module) {
-    var oop = require("ace/lib/oop");
-    var TextHighlightRules =
-      require("ace/mode/text_highlight_rules").TextHighlightRules;
+      qstring: [
+        {
+          token: "constant.language.escape",
+          regex: escapedRe,
+        },
+        {
+          token: "string",
+          regex: "\\\\$",
+          consumeLineEnd: true,
+        },
+        {
+          token: "string",
+          regex: "'|$",
+          next: "start",
+        },
+        {
+          defaultToken: "string",
+        },
+      ],
 
-    var LobsterRecordsHighlightRules = function () {
-	  this.$rules = {
-        "start": [
-          {
-		    token: "constant",
-		    regex: "[Ll]ists:|[Mm]aps:|[Vv]ariables:",
-		  },
-        ],
-      };
+      handleOpeningBrace: [
+        {
+          token: "constant.numeric",
+          regex:
+            "([0-9]{1,4}-[0-9]{1,2}-[0-9]{1,2} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}\\.{0,1}[0-9]*)",
+          next: "handleClosingBrace",
+        },
+        {
+          token: "constant.numeric",
+          regex: "(-[0-9]+|$)",
+          next: "handleClosingBrace",
+        },
+        {
+          token: "constant.numeric",
+          regex: "([0-9]+|$)",
+          next: "handleClosingBrace",
+        },
+        {
+          token: "constant.language",
+          regex: "((false|true)|$)",
+          next: "handleClosingBrace",
+        },
+        {
+          defaultToken: "text",
+        },
+      ],
+
+      handleClosingBrace: [
+        {
+          token: "keyword.operator",
+          regex: "(\\))",
+          next: "start",
+        },
+      ],
     };
+  };
 
-    oop.inherits(LobsterRecordsHighlightRules, TextHighlightRules);
+  oop.inherits(LobsterRecordsHighlightRules, TextHighlightRules);
 
-    exports.LobsterRecordsHighlightRules = LobsterRecordsHighlightRules;
-  }
-);
+  exports.LobsterRecordsHighlightRules = LobsterRecordsHighlightRules;
+});

dac0255d8ad767237b7d4c37b9461da001d1ece3
David Dodlek <david@dodlek.com>
2022-07-27 12:53:30 +0200

    chore: bump @f0rce/lit-ace version to "1.10.1"

--- src/main/java/de/f0rce/ace/AceEditor.java
+++ src/main/java/de/f0rce/ace/AceEditor.java
@@ -37,7 +37,7 @@ import de.f0rce.ace.util.AceSelection;
 /** @author David "F0rce" Dodlek */
 @SuppressWarnings("serial")
 @Tag("lit-ace")
-@NpmPackage(value = "@f0rce/lit-ace", version = "1.10.0")
+@NpmPackage(value = "@f0rce/lit-ace", version = "1.10.1")
 @JsModule("./@f0rce/lit-ace/lit-ace.js")
 public class AceEditor extends Component implements HasSize, HasStyle, Focusable<AceEditor> {
 
